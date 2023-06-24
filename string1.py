ea727a9746dea06a4071caa1e1074cdbd23ab144
Devin Dow <DevinDow@gmail.com>
2023-06-24 11:16:31 -0700

    fix_start() passes

--- string1.py
+++ string1.py
@@ -50,9 +50,8 @@ def both_ends(s):
 # Hint: s.replace(stra, strb) returns a version of string s
 # where all instances of stra have been replaced by strb.
 def fix_start(s):
-  # +++your code here+++
-  return
-
+  char = s[0:1]
+  return char + s[1:].replace(char,'*')
 
 # D. MixUp
 # Given strings a and b, return a single string with a and b separated
